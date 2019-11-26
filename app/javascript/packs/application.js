bc7923fcc0b4b11e1e8595fadb6036fe38d4c79f
abdelkrim djoudi <abdelkrim.djoudi@gmail.com>
2019-11-26 10:24:28 +0100

    sweet alert setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-btn', {
+  title: "Merci",
+  text: "Vous receverez un mail de confirmation",
+  icon: "success"
+});
