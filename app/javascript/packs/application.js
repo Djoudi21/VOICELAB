2236e5faf1e6a01303e79129356c88f7f062588f
abdelkrim djoudi <abdelkrim.djoudi@gmail.com>
2019-12-09 21:00:01 +0100

    alert after submitting form buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,17 @@
 import "bootstrap";
-import { initSweetalert } from '../plugins/init_sweetalert';
+// import swal from 'sweetalert';
+
+// import { initSweetalert } from '../plugins/init_sweetalert';
 
 
-initSweetalert('#toto', {
-  title: "Merci",
-  text: "Le contrat est validé",
-  icon: "success"
-});
-
-// initSweetalert('#sweet-alert-btn-deny', {
-//   title: "Désolé",
-//   text: "Le contrat n'est pas validé",
-//   icon: "succes"
+// initSweetalert('#toto', {
+//   title: "Merci",
+//   text: "Le contrat est validé",
+//   icon: "success"
 // });
+
+// // initSweetalert('#sweet-alert-btn-deny', {
+// //   title: "Désolé",
+// //   text: "Le contrat n'est pas validé",
+// //   icon: "succes"
+// // });
