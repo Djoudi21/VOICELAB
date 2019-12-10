27a41d48760110fc355e8c0da3d963e2240fb8d9
abdelkrim djoudi <abdelkrim.djoudi@gmail.com>
2019-12-10 16:13:53 +0100

    validations user model

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -3,7 +3,7 @@ import swal from 'sweetalert';
 const initSweetalert = (selector, options = {}) => {
   const swalButton = document.querySelector(selector);
   if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', () => {
+    swalButton.addEventListener('submit', () => {
       swal(options);
     });
   }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   # after_create :send_thanks_email
+  validates :email, :first_name, :last_name, presence: true
   validates :email, uniqueness: true
 
   private
