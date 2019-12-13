9d63597ac9ff001118115d9349d90e3ce09e8761
abdelkrim djoudi <abdelkrim.djoudi@gmail.com>
2019-12-13 13:35:27 +0100

    config smtp

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -3,7 +3,7 @@ import swal from 'sweetalert';
 const initSweetalert = (selector, options = {}) => {
   const swalButton = document.querySelector(selector);
   if (swalButton) { // protect other pages
-    swalButton.addEventListener('submit', () => {
+    swalButton.addEventListener('click', () => {
       swal(options);
     });
   }
--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module VOICELAB
   class Application < Rails::Application
     config.assets.paths << "#{Rails.root}/app/assets/videos"
 
-    config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { :api_token => ENV["POSTMARK_ID"] }
+    config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.postmark_settings = { :api_token => ENV["POSTMARK_ID"] }
 
     config.generators do |generate|
       generate.assets false
